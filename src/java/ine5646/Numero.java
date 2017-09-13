ea33d80d16fb582e90c995625329096a7cdd2bf6
Calza <Calza@NoteLipe>
2017-09-13 18:09:21 -0300

    baguncei tudo - vamos organizar parte 5

---
+++ src/java/ine5646/Numero.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ine5646;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author martin
+ */
+@ManagedBean(name = "numero")
+@RequestScoped
+public class Numero {
+    int valor;
+    String msg;
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+    
+    private boolean ehPrimo(int num){
+        boolean ret = false;
+        if (num > 1)
+        {
+            ret = true;
+            int index = 2;
+            while (ret && index < num)
+            {
+                ret = (num % index != 0);
+                index++;
+            }
+        }
+        return ret;
+    }
+    
+    public String getMsg(){
+        
+        if(ehPrimo(getValor())) {
+            return " Valor é primo";
+        } else {
+           return " Valor não é primo";
+        }
+    }
+}
